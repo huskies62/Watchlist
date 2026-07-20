f0d110fc62203286e0e85645941462ea104bf8a1
huskies62 <42459725+huskies62@users.noreply.github.com>
2026-07-19 23:26:05 -0600

    Add CapacitorHttp plugin to configuration
    Added CapacitorHttp plugin configuration to the Capacitor config.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.personal.watchlist',
   appName: 'Watchlist',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
